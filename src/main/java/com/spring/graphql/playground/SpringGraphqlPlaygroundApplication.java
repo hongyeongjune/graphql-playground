4710b4d1c71f94c5fa2e9284ab88ce55ae931d9b
hongyeongjune <hongyeongjune@naver.com>
2023-05-13 20:28:43 +0900

    feat: init

---
+++ src/main/java/com/spring/graphql/playground/SpringGraphqlPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.spring.graphql.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGraphqlPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/graphql/playground/SpringGraphqlPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.graphql.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
