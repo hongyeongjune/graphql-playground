486d5c774bdaeccbc3926384ced25dc736cddc92
hongyeongjune <hongyeongjune@naver.com>
2023-05-14 16:59:36 +0900

    feat: add ddl & dml

---
+++ src/main/resources/ddl/film.sql
@@ -0,0 +1,10 @@
+create table film (
+    file_id bigint not null auto_increment,
+    description varchar(255) not null,
+    genre varchar(255) not null,
+    poster_img varchar(255) not null,
+    release_ate date not null,
+    running_time integer not null,
+    subtitle varchar(255),
+    title varchar(255) not null, primary key (file_id)
+) engine=InnoDB;
