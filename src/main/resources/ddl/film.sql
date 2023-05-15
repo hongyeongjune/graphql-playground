eacb2a7ca54602e4fe5240c2a9db32c3b6b2951a
hongyeongjune <hongyeongjune@naver.com>
2023-05-15 22:20:06 +0900

    feat: add director value in getFilms api

--- src/main/resources/ddl/film.sql
+++ src/main/resources/ddl/film.sql
@@ -1,10 +1,12 @@
 create table film (
     file_id bigint not null auto_increment,
+    director_id bigint not null,
     description varchar(255) not null,
     genre varchar(255) not null,
     poster_img varchar(255) not null,
     release_ate date not null,
     running_time integer not null,
     subtitle varchar(255),
-    title varchar(255) not null, primary key (file_id)
+    title varchar(255) not null,
+    primary key (file_id)
 ) engine=InnoDB;
